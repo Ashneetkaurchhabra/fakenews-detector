f2ac8863b6ca5a27a0774e377d39076783936cee
Ashneetkaurchhabra <168183388+Ashneetkaurchhabra@users.noreply.github.com>
2025-11-17 23:30:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+async function analyzeNews() {
+    const text = document.getElementById("newsInput").value;
+
+    if (!text.trim()) {
+        alert("Please paste a news article!");
+        return;
+    }
+
+    const res = await fetch("http://127.0.0.1:5000/predict", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({ text })
+    });
+
+    const data = await res.json();
+
+    document.getElementById("nb").innerText = data["Naive Bayes"];
+    document.getElementById("dt").innerText = data["Decision Tree"];
+    document.getElementById("rf").innerText = data["Random Forest"];
+    document.getElementById("gb").innerText = data["Gradient Boosting"];
+    document.getElementById("stack").innerText = data["Stacking Model"];
+
+    const verdict = data["Final Verdict"];
+    const badge = document.getElementById("finalVerdict");
+
+    badge.innerText = verdict;
+
+    if (verdict === "REAL") {
+        badge.className = "verdict real";
+    } else {
+        badge.className = "verdict fake";
+    }
+
+    document.getElementById("results").classList.remove("hidden");
+}
